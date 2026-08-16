4f3df2c7b0026348aa4bdfb32f98c55f0b8131ee
Harun Gitundu <arrotechdesign@gmail.com>
2026-08-16 03:43:46 +0300

    fix: remove unused FiLinkedin import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { FiArrowUp, FiFacebook, FiInstagram, FiLinkedin, FiMail, FiMapPin, FiTwitter, FiYoutube } from 'react-icons/fi';
+import { FiArrowUp, FiFacebook, FiInstagram, FiMail, FiMapPin, FiTwitter, FiYoutube } from 'react-icons/fi';
 import { FaTiktok } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
